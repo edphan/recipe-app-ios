c3c49723eeda30d1d3e7af51080b56d943f92099
Ed <76992905+edphan@users.noreply.github.com>
2021-07-21 16:53:12 -0700

    Initial Commit

---
+++ recipe list app/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  recipe list app
+//
+//  Created by Edward Phan on 2021-07-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ recipe list app/recipe_list_appApp.swift
@@ -0,0 +1,17 @@
+//
+//  recipe_list_appApp.swift
+//  recipe list app
+//
+//  Created by Edward Phan on 2021-07-21.
+//
+
+import SwiftUI
+
+@main
+struct recipe_list_appApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
